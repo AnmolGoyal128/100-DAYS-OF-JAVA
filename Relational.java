d0b6d7d38c17e0224286d336c1016d95d9942ab2
Anmol Goyal <anmolgoyal128@gmail.com>
2026-05-15 21:38:27 +0530

    6th day of dava

---
+++ Relational.java
@@ -0,0 +1,27 @@
+public class Relational {
+    public static void main(String[] args) {
+        //Relational operators--> ==,<=,>=,!=,<,>
+        int a = 10;
+        int b = 10;
+
+        
+        boolean c = (a==b);
+        
+        boolean d = (a<=b);
+        boolean e = (a>=b);
+        boolean f = (a!=b);
+        boolean g = (a>b);
+        boolean h = (a<b);
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        
+
+        
+
+    }
+}
